8685a4ada41dec3f3713dfbe075a4739fe03a3d3
hudsonhrh <hudsonhrh7@gmail.com>
2025-01-04 22:41:53 -0500

    removed register operator

--- operator/rust/crates/operator/src/lib.rs
+++ operator/rust/crates/operator/src/lib.rs
@@ -3,7 +3,6 @@
 /// Create createNewTask at regular intervals with random task names
 pub mod spam_tasks;
 /// Register Operator and monitor for NewTaskCreated event
-pub mod start_operator;
 /// Validate signature
 pub mod validate_signature;
 /// Register Layer Operator
