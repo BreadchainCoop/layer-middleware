51a65dfe6e209dcfc5585e63ccb95700799a8d7b
rubydusa <rubydusa@protonmail.com>
2025-01-18 12:16:48 +0200

    refactor: parameterize mock token amount + log

--- contracts/script/DeployMockToken.s.sol
+++ contracts/script/DeployMockToken.s.sol
@@ -1,30 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import { Script } from "forge-std/Script.sol";
+import { Script, console } from "forge-std/Script.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
     constructor(
         string memory _name,
         string memory _symbol,
+        address _initialHolder,
         uint256 _initialSupply
     ) ERC20(_name, _symbol) {
-        _mint(msg.sender, _initialSupply);
+        _mint(_initialHolder, _initialSupply);
     }
 }
 
 contract DeployMockTokenScript is Script {
-    function run(address recipient) external {
+    function run(address recipient, uint256 amount) external {
         vm.startBroadcast();
 
         MockToken mockToken = new MockToken(
             "Mock Token",
             "MKT",
-            1_000_000 ether
+            recipient,
+            amount
         );
-        mockToken.transfer(recipient, 1_000_000 ether);
 
         vm.stopBroadcast();
+
+        console.log("MockToken deployed at:", address(mockToken));
     }
 }
