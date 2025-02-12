233d3a065e6b77169cf81c444e79f7264be3bbdf
Ron Turetzky <turetzkyron@gmail.com>
2025-02-12 10:13:36 +0100

    chore: merging upstream methods

--- contracts/src/ILayerServiceManager.sol
+++ contracts/src/ILayerServiceManager.sol
@@ -2,4 +2,14 @@
 pragma solidity ^0.8.9;
 
 interface ILayerServiceManager {
+    // ------------------------------------------------------------------------
+    // Custom Errors
+    // ------------------------------------------------------------------------
+    error InvalidSignature();
+
+    /**
+     * @param data The arbitrary data that was signed.
+     * @param signature The signature of the data.
+     */
+    function validate(bytes calldata data, bytes calldata signature) external view;
 }
